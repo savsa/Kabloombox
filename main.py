2e4985e2b8d793374713e39cc5d298e1b0344b7d
Savion Sample <savsample@gmail.com>
2018-09-24 02:04:19 -0400

    Added functionality with GCP Deferred API to do the heavy lifting in the background instead. Now stores PlaylistIDs, and Tracks without forcing the page to load

--- main.py
+++ main.py
@@ -5,11 +5,13 @@ import json
 import base64
 import os
 import time
+import logging
 
 import praw
 import config
 
 from google.appengine.api import users
+from google.appengine.ext import deferred
 from google.appengine.ext import ndb
 
 import requests_toolbelt.adapters.appengine
@@ -43,6 +45,7 @@ env = jinja2.Environment(
     autoescape=True)
 
 # Models
+
 class Track(ndb.Model):
     track_id = ndb.StringProperty()
     language = ndb.StringProperty()
@@ -56,10 +59,22 @@ class Track(ndb.Model):
     valence = ndb.FloatProperty()
     mode = ndb.IntegerProperty()
 
+class TrackID(ndb.Model):
+    value = ndb.StringProperty()
+    language = ndb.StringProperty()
+
+class PlaylistID(ndb.Model):
+    value = ndb.StringProperty()
+    language = ndb.StringProperty()
+
 # Functions
-def get_playlists_track_ids(playlist_ids, access_token):
+
+# Using the playlist ids, get all of their tracks and track_ids
+def get_playlists_track_ids(language, access_token):
     track_ids = []
-    for id in playlist_ids:
+    playlist_ids = PlaylistID.query().filter(PlaylistID.language == language)
+    for playlist_id in playlist_ids:
+        id = playlist_id.value
         playlists_tracks_link = "http://api.spotify.com/v1/playlists/{}/tracks".format(id)
         headers = { "Authorization" : "Bearer " + access_token }
         params = {
@@ -78,23 +93,36 @@ def get_playlists_track_ids(playlist_ids, access_token):
                     tracks_response = requests.get(playlists_tracks_link, headers=headers, params=params)
                     tracks_json = tracks_response.json()
                     for element in tracks_json["items"]:
-                        if element["track"]["id"] and element["track"]["id"] not in track_ids: # make sure no repeats
-                            track_ids.append(element["track"]["id"])
+                        track_ids_datastore = TrackID.query().filter(TrackID.value == element["track"]["id"]).get()
+                        if (element["track"]["id"] and element["track"]["id"] not in track_ids
+                            and not track_ids_datastore): # make sure no repeats
+                            # track_ids.append(element["track"]["id"])
+                            trackid = TrackID(value=element["track"]["id"], language=language)
+                            track_ids.append(trackid)
+                            if len(track_ids) >= 100:
+                                ndb.put_multi(track_ids)
+                                track_ids = []
+
+                            # trackid.put()
                     params["offset"] += 100
                 except KeyError: # if no track id is found
                     continue
 
-    return track_ids
+            if len(track_ids) > 0:
+                ndb.put_multi(track_ids)
 
 
-def create_tracks_from_audio_analysis(track_ids, language, access_token):
+# Retrieves the track_ids from Datastore and uses it to get the audio analyses
+# of the tracks and store that once again in Datastore
+def create_tracks_from_audio_analysis(language, access_token):
     tracks = []
-    for id in track_ids:
+    track_ids = TrackID.query().filter(TrackID.language == language)
+    for track_id in track_ids:
+        id = track_id.value
         spotify_audio_features_link = "http://api.spotify.com/v1/audio-features/{}".format(id)
         headers = { "Authorization" : "Bearer " + access_token }
         features_response = requests.get(spotify_audio_features_link, headers=headers)
         features = features_response.json()
-
         if features:
             track = Track(
                 track_id=id,
@@ -109,67 +137,98 @@ def create_tracks_from_audio_analysis(track_ids, language, access_token):
                 valence=features["valence"],
                 mode=features["mode"])
             tracks.append(track)
-        time.sleep(.8)
+            # add tracks in bulk as to not get Error 429: too many requests
+            if len(tracks) >= 100:
+                ndb.put_multi(tracks)
+                tracks = []
 
-    return tracks
+        time.sleep(1.4)
+
+    # put any remaining tracks
+    if len(tracks) > 0:
+        ndb.put_multi(tracks)
 
 
-def scan_subreddit(language, access_token):
-    reddit = praw.Reddit("bot1")
-    subreddit = reddit.subreddit(language)
-
-    playlist_ids = []
+def find_url_in_comments(playlist_ids_local, playlist_ids_datastore, subreddit, language):
+    playlist_id_objects = []
     for submission in subreddit.search("spotify"):
-        # print(submission.title.encode("utf-8"))
-        # find spotify playlist url in submission
-        new_start = 0
-        while True: # loop until all spotify links are found in submission
-            if SPOTIFY_URL in submission.selftext[new_start:]:
-                start = submission.selftext.find("playlist/", new_start) + 9
-                end = submission.selftext.find("playlist/", new_start) + 31
-                url = submission.selftext[start:end]
-                if url not in playlist_ids:
-                    playlist_ids.append(url)
-                new_start = end
-            else:
-                break
-
-        # find spotify playlist url if submission is only a link
-        if SPOTIFY_URL in submission.url:
-            start = submission.url.find("playlist/") + 9
-            end = submission.url.find("playlist/") + 31
-            url = submission.url[start:end]
-            if url not in playlist_ids:
-                playlist_ids.append(url)
-            print(url)
-
-        # find spotify playlist url in submission's comments
         submission.comments.replace_more(limit=None)
         comments = submission.comments.list()
         for comment in comments:
             new_start = 0
             while True: # loop until all spotify links are found in comment
                 if SPOTIFY_URL in comment.body[new_start:]:
-                    start = comment.body.find("playlist/", new_start) + 9
-                    end = comment.body.find("playlist/", new_start) + 31
+                    comment_body_find_playlist = comment.body.find("playlist/", new_start)
+                    start = comment_body_find_playlist + 9
+                    end = comment_body_find_playlist + 31
                     url = comment.body[start:end]
-                    if url not in playlist_ids:
-                        playlist_ids.append(url)
+                    if url not in playlist_ids_local and url not in playlist_ids_datastore:
+                        playlist_ids_local.append(url)
+                        playlistid = PlaylistID(value=url, language=language)
+                        playlist_id_objects.append(playlistid)
+                        if (playlist_id_objects >= 10):
+                            ndb.put_multi(playlist_id_objects)
+                            playlist_ids_objects = []
                     new_start = end
                 else:
                     break
-        print(playlist_ids)
-        time.sleep(3)
+
+        time.sleep(2)
+    if len(playlist_id_objects) > 0:
+        ndb.put_multi(playlist_id_objects)
+
+
+# Scrape specified subreddit for all Spotify links, and use that data to get
+# playlist ids, track ids, and audio analyses of each track
+def scan_subreddit(language, access_token):
+    reddit = praw.Reddit("bot1")
+    subreddit = reddit.subreddit(language)
+    playlist_ids_local = []
+    playlist_ids_datastore = []
+    for playlist_id in PlaylistID.query().filter(PlaylistID.language == language):
+        playlist_ids_datastore.append(playlist_id.value)
+
+    # find spotify playlist url in submission
+    for submission in subreddit.search("spotify"):
+        new_start = 0
+        while True: # loop until all spotify links are found in submission
+            if SPOTIFY_URL in submission.selftext[new_start:]:
+                submission_text_find_playlist = submission.selftext.find("playlist/", new_start)
+                start = submission_text_find_playlist + 9
+                end = submission_text_find_playlist + 31
+                url = submission.selftext[start:end]
+                if url not in playlist_ids_local and url not in playlist_ids_datastore:
+                    playlist_ids_local.append(url)
+                    playlistid = PlaylistID(value=url, language=language)
+                    playlistid.put()
+                new_start = end
+            else:
+                break
+
+    # find spotify playlist url if submission is only a link
+    for submission in subreddit.search("spotify"):
+        if SPOTIFY_URL in submission.url:
+            submission_url_find_playlist = submission.url.find("playlist/", new_start)
+            start = submission_url_find_playlist + 9
+            end = submission_url_find_playlist + 31
+            url = submission.url[start:end]
+            if url not in playlist_ids_local and url not in playlist_ids_datastore:
+                playlist_ids_local.append(url)
+                playlistid = PlaylistID(value=url, language=language)
+                playlistid.put()
+
+    # find spotify playlist url in comments of all submissions
+    deferred.defer(find_url_in_comments, playlist_ids_local, playlist_ids_datastore, subreddit, language)
 
     # get a playlist's tracks
-    track_ids = get_playlists_track_ids(playlist_ids, access_token)
+    deferred.defer(get_playlists_track_ids, language, access_token)
     # get analysis of each track using the tracks' ids
-    tracks = create_tracks_from_audio_analysis(track_ids, language, access_token)
-
-    return tracks
+    deferred.defer(create_tracks_from_audio_analysis, language, access_token)
+    logging.debug("tracks got")
 
 
 # Pages
+
 class HomePage(webapp2.RequestHandler):
     def get(self):
         template = env.get_template("templates/home.html")
@@ -179,7 +238,6 @@ class HomePage(webapp2.RequestHandler):
 class Search(webapp2.RequestHandler):
     def get(self):
         code = self.request.get("code")
-
         template_vars = {
             "code" : code,
         }
@@ -199,7 +257,6 @@ class Search(webapp2.RequestHandler):
             }
 
             headers = { "Authorization" : "Basic " + base64.b64encode(CLIENT_ID + ":" + CLIENT_SECRET) }
-
             response = requests.post(SPOTIFY_TOKEN_URL, data=payload, headers=headers)
             token = response.json()
 
@@ -207,8 +264,7 @@ class Search(webapp2.RequestHandler):
                 access_token = token["access_token"]
                 refresh_token = token["refresh_token"]
 
-                tracks = scan_subreddit(language, access_token)
-                print("LENGTH OF TRACKS: " + str(len(tracks)))
+                scan_subreddit(language, access_token)
 
         template = env.get_template("templates/search.html")
         self.response.write(template.render())
